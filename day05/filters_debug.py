9f9dffd5417df3e4f0bec0e7b3c1b94dd6325d3f
Kimberly Head <khead@my.strayer.edu>
2026-01-12 21:31:09 -0600

    an unsaved file

--- day05/filters_debug.py
+++ day05/filters_debug.py
@@ -3,10 +3,10 @@ def filter_users(users, *, min_age=None, status=None):
     for user in users:
         matches = True
 
-        if min_age is not None and user["age"] < min_age:
+        if min_age is not None and user["age"] < min_age:   # changed from <= to < because it is a minimum age
             matches = False
 
-        if status is not None and user["status"] != status:
+        if status is not None and user["status"] != status:  # changed from = to != because if it is equal it should be true
             matches = False
 
         if matches:
